1b420da4a878a283a03b854cd3972bfa45ecd7dd
Daria Marczak <marczak.ria@gmail.com>
2018-07-06 16:35:26 +0100

    Added first saga

--- src/getStore.js
+++ src/getStore.js
@@ -5,12 +5,13 @@ import {
 } from 'redux';
 
 import { createLogger } from 'redux-logger';
-import { Iterable } from 'immutable'
-import thunk from 'redux-thunk'
-
-import { getQuery } from './utility'
+import { Iterable } from 'immutable';
+import thunk from 'redux-thunk';
+import { getQuery } from './utility';
 import { reducer } from './combineReducers';
-import { defaultState } from './defaultState'
+import createSagaMiddleware from "redux-saga";
+import { defaultState } from './defaultState';
+import { initSaga } from "./initSaga";
 
 const stateTransformer = (state) => {
     if (Iterable.isIterable(state)) return state.toJS();
@@ -22,7 +23,8 @@ const logger = createLogger({
 });
 
 export const getStore = ()=>{
-    const middleWares = [thunk];
+    const sagaMiddleware = createSagaMiddleware();
+    const middleWares = [sagaMiddleware, thunk];
     if (getQuery()['logger']) { middleWares.push(logger)}
     const composables = [applyMiddleware(...middleWares)]
     const enhancer = compose(
@@ -33,5 +35,6 @@ export const getStore = ()=>{
         defaultState,
         enhancer
     );
+    initSaga(sagaMiddleware);
     return store;
 };
